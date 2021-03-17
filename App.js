af6c5aa50d65f1e32ef6479e63f140b7a1b0d3ce
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-03-16 22:32:31 -0400

    added button and text input

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ padding: 50 }}>
       <View>
-
+        <TextInput placeholder="Course Goal" style={{ borderBottomColor: "black", borderBottomWidth: 1, height: 40 }} />
+        <Button title="ADD" />
       </View>
       <View>
 
